bd857b66c832f93f96320977a4e9f59de3f43a59
Milwall <Milwall@users.noreply.github.com>
2014-11-14 15:32:13 +0000

    Create Test Poker.java

---
+++ Test Poker.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keith_milton_sb13516_ap;
+
+/**
+ *
+ * @author Keith Milton
+ */
+public class TestPoker
+{
+
+    public static void main(String[] args)
+    {
+        //creates a new player when main method is run
+        Player player1 = new Player();
+    }
+}
